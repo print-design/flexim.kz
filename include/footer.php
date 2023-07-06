9e0bf38b2ed63420182980171258352041137b9f
user <user@Dmitry>
2023-07-06 16:48:57 +0300

    06.07.2023 дизайн

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,18 @@
-<footer></footer>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-2">
+                <i class="far fa-copyright mr-3"></i>FLEXIM
+            </div>
+            <div class="col-12 col-lg-2">
+                <a href="<?=APPLICATION ?>/">Главная</a>
+            </div>
+            <div class="col-12 col-lg-2">
+                <a href="<?=APPLICATION ?>/contact.php">Контакты</a>
+            </div>
+        </div>
+    </div>
+</footer>
 <script src="<?=APPLICATION ?>/js/jquery-3.5.1.min.js"></script>
 <script src="<?=APPLICATION ?>/js/bootstrap.min.js"></script>
 <script src="<?=APPLICATION ?>/js/jquery.maskedinput.js"></script>
